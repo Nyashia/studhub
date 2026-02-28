62c6ed52251e59a987fd130ab78dde5981361ddf
Nyashia . <NyashiaCarrington0@gmail.com>
2026-02-28 10:56:04 +0800

    Established basic task and user routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+
+///////////get all uers
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+///////////get a uer by id
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+///////////////// update a user
+app.put('/users/:id', async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body, 
+      { new: true } 
+    );
+    if (!updatedUser) return res.status(404).json({ message: "User not found" });
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+//////////////delete a user
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) return res.status(400).json({ message: "User not found" });
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
+
+
+//post route for users
+app.post('/users', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const newUser = new User({
+      name,
+      email,
+      password
+    });
+
+    const savedUser = await newUser.save();
+
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
